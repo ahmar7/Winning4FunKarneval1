36978ecb59d68a13a4362168b47c97380521d23c
ahmar7 <ahmarjabbar7@gmail.com>
2024-04-15 23:10:31 +0500

    new

---
+++ hs.js
@@ -0,0 +1,52 @@
+!(function (e, t) {
+  if (!document.getElementById(e)) {
+    var c = document.createElement("script");
+    (c.src = "https://js.hs-analytics.net/analytics/1713204300000/45590864.js"),
+      (c.type = "text/javascript"),
+      (c.id = e);
+    var n = document.getElementsByTagName("script")[0];
+    n.parentNode.insertBefore(c, n);
+  }
+})("hs-analytics");
+!(function (t, e, r) {
+  if (!document.getElementById(t)) {
+    var n = document.createElement("script");
+    for (var a in ((n.src =
+      "https://js.hscollectedforms.net/collectedforms.js"),
+    (n.type = "text/javascript"),
+    (n.id = t),
+    r))
+      r.hasOwnProperty(a) && n.setAttribute(a, r[a]);
+    var i = document.getElementsByTagName("script")[0];
+    i.parentNode.insertBefore(n, i);
+  }
+})("CollectedForms-45590864", 0, {
+  crossorigin: "anonymous",
+  "data-leadin-portal-id": 45590864,
+  "data-leadin-env": "prod",
+  "data-loader": "hs-scriptloader",
+  "data-hsjs-portal": 45590864,
+  "data-hsjs-env": "prod",
+  "data-hsjs-hublet": "na1",
+});
+var _hsp = (window._hsp = window._hsp || []);
+_hsp.push(["addEnabledFeatureGates", []]);
+!(function (t, e, r) {
+  if (!document.getElementById(t)) {
+    var n = document.createElement("script");
+    for (var a in ((n.src = "https://js.hs-banner.com/v2/45590864/banner.js"),
+    (n.type = "text/javascript"),
+    (n.id = t),
+    r))
+      r.hasOwnProperty(a) && n.setAttribute(a, r[a]);
+    var i = document.getElementsByTagName("script")[0];
+    i.parentNode.insertBefore(n, i);
+  }
+})("cookieBanner-45590864", 0, {
+  "data-cookieconsent": "ignore",
+  "data-hs-ignore": true,
+  "data-loader": "hs-scriptloader",
+  "data-hsjs-portal": 45590864,
+  "data-hsjs-env": "prod",
+  "data-hsjs-hublet": "na1",
+});
